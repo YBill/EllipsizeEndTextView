1fac4ca642130e3f68d014e07f508b72ef18592b
weibiao <ybilland@163.com>
2018-09-21 16:39:10 +0800

    demo

--- app/src/main/java/com/bill/ellipsizeendtextview/MainActivity.java
+++ app/src/main/java/com/bill/ellipsizeendtextview/MainActivity.java
@@ -5,15 +5,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EllipsizeEndTextView content;
+    private EllipsizeEndTextView content1;
+    private EllipsizeEndTextView content2;
+    private EllipsizeEndTextView content3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        content = findViewById(R.id.text_view);
-        content.setText("据中新网消息，近日不少市民反映多数快递员都存在不送货上门的情况，为了省事，直接将包裹放在快递柜里。" +
-                "而快递柜又都从免费变为收费了。这些快递柜收费的形式各不相同。有的快递柜是双向收费，有的是单向收费。" +
-                "例如，丰巢快递柜就是双向收费模式，即快递员存件要付费，用户超过 4 小时未取支付 1 元。而云柜则是快递员寄件收费，用户取件免费的模式。");
+        content1 = findViewById(R.id.text_view_1);
+        content2 = findViewById(R.id.text_view_2);
+        content3 = findViewById(R.id.text_view_3);
+
+        content1.setText(getString(R.string.text_1));
+
+        content2.setText(getString(R.string.text_2));
+
+        content3.setMaxLine(4);
+        content3.setText(getString(R.string.text_3));
     }
 }
